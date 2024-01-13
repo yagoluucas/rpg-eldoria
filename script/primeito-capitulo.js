9556b06a583dddeaec4c0d751aeee53433d5c569
yagoluucas <yago543@gmail.com>
2024-01-13 10:35:09 -0300

    colocado informações sobre vilão do RPG

--- script/primeito-capitulo.js
+++ script/primeito-capitulo.js
@@ -1,19 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
     let nomeUsuario = '';
     const dialogoEscolha = document.querySelector('h1.dialogo-escolha')
-    const btnComecarJornada = document.querySelector('.js-comecar-jornada-btn');
+    const btnComecarJornada = document.querySelector('button.js-comecar-jornada-btn');
     const inputInfo = document.querySelector("input.js-input-info");
     const btnAvancar = document.querySelector('button.js-btn-avancar');
     const paragrafoPrimeiraEscolha = document.querySelector('.js-primeira-escolha');
     const paragrafoSegundaEscolha = document.querySelector('.js-segunda-escolha');
     const personagem = document.querySelector('.js-personagem')
     const body = document.body;
-    // let audio = new Audio('../audio/som-floresta.mp3')
-    // const audioPlay = setInterval(() => {
-    //     audio.play()
-    // }, 0.000000000001)
     function dialogoInicial() {       
-        // verifica se o nome tem mais de 2 caracter e se não tem numero
         if (inputInfo.value.length <= 2 || !(isNaN(inputInfo.value + 1))) {
             alert('Por favor, é necessario informar um nome valido')
         }
@@ -31,8 +26,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 paragrafoSegundaEscolha.classList.remove('none')
                 paragrafoSegundaEscolha.textContent = 'Viajante intrépido, em Eldoria, onde elfos, anões e fadas tecem o cenário mágico, uma antiga sombra ameaça o equilíbrio. Tu, escolhido, és chamado para enfrentar o mal que se ergue. Com a aliança de criaturas lendárias como centauros, grifos e ninfas, tua jornada decidirá o destino desse reino. Ergue-te, pois a esperança de Eldoria repousa em teu coração corajoso.'
                 paragrafoSegundaEscolha.classList.add('animacao-esquerda')
-
                 setTimeout(() => {
+                    btnComecarJornada.textContent = 'Começar Jornada'
                     btnComecarJornada.classList.remove('none')
                     btnComecarJornada.classList.add('animacao-aparecer')
                 }, 4000)
@@ -47,43 +42,56 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     introducao()
 
-    // abaixo está a segunda parte do capitulo
     function historiaMalvagor() {
-        // audio.pause()
-        // clearInterval(audioPlay)
-        let audio = new Audio('../audio/som-terror.mp3')
         audio.play()
         btnAvancar.classList.add('none')
-        body.style.backgroundColor = '#000000'
-        body.style.backgroundImage = 'none'
-        personagem.setAttribute('src', 'image/rosto-malfagor.webp')
-        personagem.classList.remove('animacao-subindo')
-        personagem.classList.add('animacao-aparecer')
-        personagem.style.marginTop = '30px'
+            body.style.backgroundColor = '#000000'
+            body.style.backgroundImage = 'none'
+            personagem.setAttribute('src', 'image/rosto-malfagor.webp')
+            personagem.classList.remove('animacao-subindo')
+            personagem.classList.add('animacao-aparecer')
+            personagem.style.marginTop = '30px'
+            dialogoEscolha.classList.add('none')
+            paragrafoSegundaEscolha.classList.remove('animacao-esquerda')
+            paragrafoPrimeiraEscolha.textContent = 'Malvagor, sombrio por natureza, emerge das profundezas do desconhecido. Uma vez um ser de luz corrompido por ambições desmedidas, sua busca por poder desencadeou uma onda de trevas sobre Eldoria. Utilizando artes proibidas, despertou criaturas antigas e enegreceu corações leais'
+            paragrafoSegundaEscolha.textContent = ''
+            setTimeout(() => {
+                paragrafoSegundaEscolha.classList.add('animacao-aparecer')
+                paragrafoSegundaEscolha.textContent = 'Assolou nossas terras, desafiando a harmonia que reinava entre elfos, anões e fadas. Com suas artimanhas, desencadeou conflitos e semeou discórdia, tornando-se a personificação da sombra que paira sobre Eldoria'
+            }, 4000)
+
     }
     btnComecarJornada.addEventListener('click', () => {
-        if (!(nomeUsuario !== undefined)) {
-            alert('Por favor, é necessario informar um nome!')
-        } else {
+            paragrafoPrimeiraEscolha.classList.remove('animacao-aparecer')
+            paragrafoSegundaEscolha.classList.remove('animacao-esquerda')
             btnAvancar.removeEventListener('click', dialogoInicial)
+            dialogoEscolha.style.fontSize = '1.6rem';
+            dialogoEscolha.classList.add('animacao-esquerda')
             dialogoEscolha.innerHTML = 'Como informei anteriormente, sou o mestre dos magos, meu nome é <span class="Althor-nome">Althor</span> guardião da antiga sabedoria e condutor de tua jornada épica'
             inputInfo.classList.add('none')
             btnComecarJornada.remove()
             paragrafoSegundaEscolha.textContent = ''
             paragrafoPrimeiraEscolha.textContent = ''
             setTimeout(() => {
+                paragrafoPrimeiraEscolha.classList.add('animacao-esquerda')
                 paragrafoPrimeiraEscolha.textContent = 'Nobre viajante, nossa terra era feliz, onde seres místicos brincavam e confraternizavam pelos campos'
                 setTimeout(() => {
+                    paragrafoSegundaEscolha.classList.add('animacao-esquerda')
                     paragrafoSegundaEscolha.innerHTML = 'Contudo, a alegria foi interrompida pela chegada de um mal ancestral, conhecido como <span class="nome-vilao">Malvagor</span>'
                     setTimeout(() => {
                         btnAvancar.classList.remove('none')
+                        btnAvancar.classList.add('btn-avancar')
+                        btnAvancar.classList.add('animacao-aparecer')
                         btnAvancar.textContent = 'Ver historia de Malvagor'
-                        btnAvancar.addEventListener('click', historiaMalvagor)
-                    })
-                }, 1000)
-            }, 1000)
+                        btnAvancar.style.backgroundColor = '#FF0000'
+                        let audio = new Audio('../audio/som-terror.mp3')
+                        btnAvancar.addEventListener('click', function() {
+                            historiaMalvagor(audio)
+                        })
+                    }, 3000)
+                }, 3000)
+            }, 3000)
 
-        }
-    })
+        })
 
 })
